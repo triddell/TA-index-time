f87c467538937b14d3d71891c97c20fe42501a17
Tim Riddell <tim@riddell.us>
2019-08-16 09:15:35 -0600

    add: format sourcetype column in README

--- gen/gen-index-time-table.py
+++ gen/gen-index-time-table.py
@@ -14,7 +14,7 @@ with open('gen/index-time-props.csv') as csvfile:
 
     for row in reader:
 
-        table += "| %s | %s | %s | %s | %s |\n" % (esc_bs(row['SOURCETYPE']), esc_bs(row['EXAMPLE']), esc_bs(row['TIME_PREFIX']), '`'+esc_bs(row['TIME_FORMAT'])+'`', '`'+esc_bs(row['LINE_BREAKER'])+'`')
+        table += "| %s | %s | %s | %s | %s |\n" % ('`'+esc_bs(row['SOURCETYPE'])+'`', esc_bs(row['EXAMPLE']), esc_bs(row['TIME_PREFIX']), '`'+esc_bs(row['TIME_FORMAT'])+'`', '`'+esc_bs(row['LINE_BREAKER'])+'`')
 
 with fileinput.FileInput('README.md', inplace=True) as file:
     for line in file:
